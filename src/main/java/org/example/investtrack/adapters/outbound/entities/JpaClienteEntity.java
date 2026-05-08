13396af154d3fbf4ec74117aff01eb9e25fdf5da
ValmirboJr <valmirborgesojr@gmail.com>
2026-05-08 09:26:33 -0300

    feat:Criação da camada responsavel pela entidade JPA do Cliente

--- src/main/java/org/example/investtrack/adapters/outbound/entities/JpaClienteEntity.java
+++ src/main/java/org/example/investtrack/adapters/outbound/entities/JpaClienteEntity.java
@@ -1,4 +1,31 @@
 package org.example.investtrack.adapters.outbound.entities;
 
-public class JPAClienteEntity {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.example.investtrack.Domain.model.cliente.Cliente;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "cliente")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class JpaClienteEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String nome;
+    private String cpf;
+    private String email;
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    private LocalDateTime data_cadastro;
+
 }
