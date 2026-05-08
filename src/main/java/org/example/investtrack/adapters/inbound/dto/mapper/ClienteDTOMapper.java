89f84c1f0dc1d489e6e7bb37a1b47a8341945a75
ValmirboJr <valmirborgesojr@gmail.com>
2026-05-08 09:26:07 -0300

    feat:Criação da camada responsavel pelo mapeamento dos dados do Cliente

--- src/main/java/org/example/investtrack/adapters/inbound/dto/mapper/ClienteDTOMapper.java
+++ src/main/java/org/example/investtrack/adapters/inbound/dto/mapper/ClienteDTOMapper.java
@@ -1,4 +1,35 @@
 package org.example.investtrack.adapters.inbound.dto.mapper;
 
-public class ClienteDTOmapper {
+import org.example.investtrack.Domain.model.cliente.Cliente;
+import org.example.investtrack.adapters.inbound.dto.ClienteRequestDTO;
+import org.example.investtrack.adapters.inbound.dto.ClienteResponseDTO;
+
+import java.util.List;
+
+public class ClienteDTOMapper {
+
+    public static Cliente toDomain(ClienteRequestDTO request) {
+        return Cliente.criar(
+                request.nome(),
+                request.cpf(),
+                request.email(),
+                request.data_cadastro()
+        );
+    }
+
+    public static ClienteResponseDTO fromDomain(Cliente domain) {
+        return new ClienteResponseDTO(
+                domain.getId(),
+                domain.getNome(),
+                domain.getCpf(),
+                domain.getEmail(),
+                domain.getData_cadastro()
+        );
+    }
+
+    public static List<ClienteResponseDTO> fromDomain(List<Cliente> clientes) {
+        return clientes.stream()
+                .map(ClienteDTOMapper::fromDomain)
+                .toList();
+    }
 }
