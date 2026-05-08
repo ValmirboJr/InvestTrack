db115b248220fe9a46f6f91f5b96832c459b9d7c
ValmirboJr <valmirborgesojr@gmail.com>
2026-05-08 09:28:29 -0300

    feat: Criação do repositório JPA do cliente

--- src/main/java/org/example/investtrack/adapters/outbound/repositories/JpaClienteRespository.java
+++ src/main/java/org/example/investtrack/adapters/outbound/repositories/JpaClienteRespository.java
@@ -1,4 +1,9 @@
 package org.example.investtrack.adapters.outbound.repositories;
 
-public interface JPAClienteRespository {
+import org.example.investtrack.adapters.outbound.entities.JpaClienteEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface JpaClienteRespository extends JpaRepository<JpaClienteEntity, UUID> {
 }
