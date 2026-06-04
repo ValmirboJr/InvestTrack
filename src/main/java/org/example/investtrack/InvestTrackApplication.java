d3724634a04bd6f8b6d78e608f9154acc39ec746
ValmirboJr <valmirborgesojr@gmail.com>
2026-06-04 03:51:47 -0300

    style: ajusta indentacao da aplicacao

--- src/main/java/org/example/investtrack/InvestTrackApplication.java
+++ src/main/java/org/example/investtrack/InvestTrackApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class InvestTrackApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(InvestTrackApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(InvestTrackApplication.class, args);
+    }
 
 }
--- src/test/java/org/example/investtrack/InvestTrackApplicationTests.java
+++ src/test/java/org/example/investtrack/InvestTrackApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class InvestTrackApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
